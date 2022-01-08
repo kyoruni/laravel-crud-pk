7f2dd9c147e4af461ff9eb710631809e56ee05c0
kyoruni <kyoruni@gmail.com>
2022-01-08 23:08:15 +0900

    :wrench: #2 types テーブル作成

--- database/migrations/2022_01_08_134307_create_types_table.php
+++ database/migrations/2022_01_08_134307_create_types_table.php
@@ -14,7 +14,8 @@ class CreateTypesTable extends Migration
     public function up()
     {
         Schema::create('types', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('id')->comment('ID');
+            $table->string('name', 30)->comment('名前');
             $table->timestamps();
         });
     }
