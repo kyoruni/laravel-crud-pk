b8c60b0c189d006222afac392f554d0820b922c1
kyoruni <kyoruni@gmail.com>
2022-01-08 23:30:27 +0900

    #2 :wrench: pokemons テーブル修正

--- database/migrations/2022_01_08_141216_create_pokemons_table.php
+++ database/migrations/2022_01_08_141216_create_pokemons_table.php
@@ -14,7 +14,12 @@ class CreatePokemonsTable extends Migration
     public function up()
     {
         Schema::create('pokemons', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('id')->comment('ID');
+            $table->string('name', 30)->comment('名前');
+            $table->unsignedInteger('no')->unique()->comment('図鑑No.');
+            $table->string('category', 30)->comment('分類');
+            $table->unsignedDecimal('height', 4, 1)->comment('高さ');
+            $table->unsignedDecimal('weight', 4, 1)->comment('重さ');
             $table->timestamps();
         });
     }
