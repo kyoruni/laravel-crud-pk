0c87357d0b08512de899a26377e51f92e9863262
kyoruni <kyoruni@gmail.com>
2022-01-09 00:10:17 +0900

    #2 :wrench: pokemon_type テーブル修正

--- database/migrations/2022_01_08_143329_create_pokemon_type_table.php
+++ database/migrations/2022_01_08_143329_create_pokemon_type_table.php
@@ -14,8 +14,15 @@ class CreatePokemonTypeTable extends Migration
     public function up()
     {
         Schema::create('pokemon_type', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('id')->comment('ID');
+            $table->unsignedBigInteger('pokemon_id')->comment('ポケモンID');
+            $table->unsignedBigInteger('type_id')->comment('タイプID');
             $table->timestamps();
+            // ポケモンとタイプの組み合わせは重複しない
+            $table->unique(['pokemon_id', 'type_id']);
+            // 外部キー制約
+            $table->foreign('pokemon_id')->references('id')->on('pokemons')->onDelete('cascade');
+            $table->foreign('type_id')->references('id')->on('types');
         });
     }
 
