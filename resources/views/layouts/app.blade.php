85a78ae7d30fe3f69820c1f2b12504e8583276e7
kyoruni <kyoruni@gmail.com>
2022-01-12 21:32:15 +0900

    #8 :wrench: ヘッダーナビ作成

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,30 @@
         <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
     </head>
     <body>
+        <nav class="navbar navbar-expand-lg navbar-dark navbar-info bg-info">
+            <a class="navbar-brand" href="/">ホーム</a>
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/pokemons">ポケモン</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/types">タイプ</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
         <div class="container-fluid bg-info">
             <div class="row">
                 <div class="col-10 mx-auto bg-success">
